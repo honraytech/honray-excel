242a43b50edb599d535505d2f5aa621240cccbdf
honraytech <tech@honraytech.com>
2017-01-06 09:13:07 +0800

    修改目录结构

--- src/PHPExcel.php
+++ src/PHPExcel.php
@@ -25,13 +25,13 @@
  * @version    ##VERSION##, ##DATE##
  */
 
+namespace think\excel;
 
 /** PHPExcel root directory */
 if (!defined('PHPEXCEL_ROOT')) {
     define('PHPEXCEL_ROOT', dirname(__FILE__) . '/');
     require(PHPEXCEL_ROOT . 'PHPExcel/Autoloader.php');
 }
-
 
 /**
  * PHPExcel
